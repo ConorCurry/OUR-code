a5d218b9ef41efac7fb8a47bb71b954729ce1585
ConorCurry <conorcurry@gmail.com>
2017-01-26 12:08:37 -0500

    Bugs should be fixed. Near final version. Added error handling for downloads

--- LearningAgreements/agreementsDownload.py
+++ LearningAgreements/agreementsDownload.py
@@ -7,7 +7,8 @@ def download_doc(fn, folder, url):
     _, extension = os.path.splitext(url)
     fn += extension
     fn = os.path.join(folder, fn)
-    r = requests.get(url, auth=auth)
+    print(repr(url))
+    r = requests.get(url)
     with open(fn, "wb") as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:
@@ -19,7 +20,6 @@ with open("API.key", "r") as f:
     user = f.readline().strip()
 password = 'notused'
 form_hash = 'z1p6grwu02kuwi7'
-
 auth = (user, password)
 count_url = base_url+'forms/{}/entries/count.json'.format(form_hash)
 entries_url = base_url+'forms/{}/entries.json'.format(form_hash)
@@ -36,23 +36,38 @@ while (entry_count//100) > 0:
 r = requests.get(entries_url.format(len(entries), entry_count%100), auth=auth)
 entries.extend(r.json()['Entries'])
 entry_count = 0
-entries.reverse()
+print("Entries remaining: {}".format(entry_count))
 
+entries.reverse()
 last = len(entries) + 1
 processed_students = set()
+download_errors = []
+error_file = 'errors.txt'
 print("Downloading Documents...")
 for e in entries:
     assert int(e['EntryId']) < last
     last = int(e['EntryId'])
-    name = '{}_{}'.format(e['Field15'].strip(), e['Field14'].strip()).lower()
-    
+    name = '{}_{}'.format(e['Field15'].strip(), e['Field14'].strip()).lower()  
+    name.replace(' ', '-')  
     if name in processed_students:
         print('Skipping duplicate\tName: {}\tentry#: {}'.format(name, e['EntryId']))
         continue
     else:
         print('Saving Document\tName: {}\tentry#: {}'.format(name, e['EntryId']))        
         processed_students.add(name)
-        download_doc(name, "downloaded_agreements", e['Field21'])
+        #remove text cruft from url
+        url = e['Field21'].split('(')[1][:-1]
+        try:
+            download_doc(name, "downloaded_agreements", url)
+        except:
+            download_errors.append(e)
+            print("ERROR downloading agreement for {}".format(name)
+with open(error_file, 'w') as f:
+    for line in download_errors:
+        f.write(line + '\n')
+        
+print('{} Attempted Downloads, {} Download Errors.'.format(len(processed_students), len(download_errors))
+print('Check {} for info on failed downloads'.format(error_file))
         
 
         
