53be81a28aa8fb2df6db4cb47e0add1af3ba386f
ConorCurry <conorcurry@gmail.com>
2017-01-27 01:16:58 -0500

    Added check to not redownload if unnecessary. Tested & worked as expected

--- LearningAgreements/agreementsDownload.py
+++ LearningAgreements/agreementsDownload.py
@@ -2,6 +2,7 @@
 
 import requests
 import os
+import pickle
 
 def download_doc(fn, folder, url):
     _, extension = os.path.splitext(url)
@@ -25,48 +26,67 @@ count_url = base_url+'forms/{}/entries/count.json'.format(form_hash)
 entries_url = base_url+'forms/{}/entries.json'.format(form_hash)
 entries_url += '?pageStart={}&pageSize={}'
 
+print("Getting submission entries from Wufoo...")
+
 entry_count = int(requests.get(count_url, auth=auth).json()['EntryCount'])
 entries = []
-
+print("\rEntries remaining: {}".format(entry_count))
 while (entry_count//100) > 0:
     r = requests.get(entries_url.format(len(entries), 100), auth=auth)
     entries.extend(r.json()['Entries'])
     entry_count -= 100
-    print("Entries remaining: {}".format(entry_count))
+    print("\rEntries remaining: {}".format(entry_count))
 r = requests.get(entries_url.format(len(entries), entry_count%100), auth=auth)
 entries.extend(r.json()['Entries'])
 entry_count = 0
-print("Entries remaining: {}".format(entry_count))
+print("\rEntries remaining: {}".format(entry_count))
 
 entries.reverse()
 last = len(entries) + 1
 processed_students = set()
 download_errors = []
 error_file = 'errors.txt'
+dloaded_file = 'dloaded.txt'
+try:
+    with open(dloaded_file, 'rb') as handle:
+        dloaded = pickle.loads(handle.read())
+except FileNotFoundError:
+    dloaded = {}
+    
 print("Downloading Documents...")
-for e in entries:
-    assert int(e['EntryId']) < last
-    last = int(e['EntryId'])
-    name = '{}_{}'.format(e['Field15'].strip(), e['Field14'].strip()).lower()  
-    name.replace(' ', '-')  
-    if name in processed_students:
-        print('Skipping duplicate\tName: {}\tentry#: {}'.format(name, e['EntryId']))
-        continue
-    else:
-        print('Saving Document\tName: {}\tentry#: {}'.format(name, e['EntryId']))        
-        processed_students.add(name)
-        #remove text cruft from url
-        url = e['Field21'].split('(')[1][:-1]
-        try:
-            download_doc(name, "downloaded_agreements", url)
-        except:
-            download_errors.append(e)
-            print("ERROR downloading agreement for {}".format(name)
-with open(error_file, 'w') as f:
-    for line in download_errors:
-        f.write(line + '\n')
+try:
+    for e in entries:
+        assert int(e['EntryId']) < last
+        last = int(e['EntryId'])
+        name = '{}_{}'.format(e['Field15'].strip(), e['Field14'].strip()).lower()  
+        name = name.replace(' ', '-')  
+        if name in processed_students:
+            print('Skip Duplicate\tName: {:25}\tentry#: {:5}'.format(name, e['EntryId']))
+            continue
+        else:       
+            processed_students.add(name)
+            #remove text cruft from url
+            url = e['Field21'].split('(')[1][:-1]
+            #try:
+            if name not in dloaded or dloaded[name] != e['EntryId']:
+                download_doc(name, "downloaded_agreements", url)
+                dloaded[name] = e['EntryId']
+                print('Saving Document\tName: {:25}\tentry#: {:5}'.format(name, e['EntryId']))
+            else:
+                print('Document Exists\tName: {:25}\tentry#: {:5}'.format(name, e['EntryId']))
+            #except:
+            #    download_errors.append(e)
+            #    print("ERROR downloading agreement for {}".format(name))
+except KeyboardInterrupt:
+    print("Ending program...")
+finally:
+    #with open(error_file, 'w') as f:
+    #    for line in download_errors:
+    #        f.write(line + '\n')
+    with open(dloaded_file, 'wb') as f:
+        pickle.dump(dloaded, f)
         
-print('{} Attempted Downloads, {} Download Errors.'.format(len(processed_students), len(download_errors))
+print('{} Attempted Downloads, {} Download Errors.'.format(len(processed_students), len(download_errors)))
 print('Check {} for info on failed downloads'.format(error_file))
         
 
